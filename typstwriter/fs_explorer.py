26eda243cfaa9b3587a6b6d904c230d47d46b2d8
Bzero <B_0@posteo.net>
2024-04-07 18:49:58 +0200

    Add ContextMenu to FSExplorer.

--- typstwriter/fs_explorer.py
+++ typstwriter/fs_explorer.py
@@ -15,6 +15,95 @@ config = configuration.Config
 state = globalstate.State
 
 
+class FSContextMenu(QtWidgets.QMenu):
+    """
+    Base context menu for the fs Explorer.
+
+    This class contains all actions and handlers that may be needed in a FS context menu.
+    It does not actually construct a menu however, this is done in a subclass which can
+    use a selection of the actions declared in this clas to populate the menu.
+    """
+
+    def __init__(self, parent):
+        """Declare all actions."""
+        QtWidgets.QMenu.__init__(self, parent)
+        self.context_path = None
+
+        self.action_open = QtWidgets.QAction("Open", triggered=self.handle_open)
+        self.action_new_file = QtWidgets.QAction("New file", triggered=self.handle_new_file)
+        self.action_new_folder = QtWidgets.QAction("New folder", triggered=self.handle_new_folder)
+        self.action_rename = QtWidgets.QAction("Rename", triggered=self.handle_rename)
+        self.action_delete = QtWidgets.QAction("Delete", triggered=self.handle_delete)
+        self.setRoot = QtWidgets.QAction("Set as Working Directory", triggered=self.handle_set_root)
+        self.action_copy_path = QtWidgets.QAction("Copy path", triggered=self.handle_copy_path)
+
+    def handle_open(self):
+        """Trigger opening a file."""
+        self.parent().open_file.emit(self.context_path)
+
+    def handle_new_file(self):
+        """Trigger creating a new file."""
+        self.parent().new_file_in_dir(self.context_path)
+
+    def handle_new_folder(self):
+        """Trigger creating a new folder."""
+        self.parent().new_folder_in_dir(self.context_path)
+
+    def handle_rename(self):
+        """Trigger renaming folder."""
+        self.parent().rename_from(self.context_path)
+
+    def handle_delete(self):
+        """Trigger deleteing file or folder."""
+        self.parent().delete(self.context_path)
+
+    def handle_set_root(self):
+        """Trigger setting the current folder as working directory."""
+        self.parent().set_root(self.context_path)
+
+    def handle_copy_path(self):
+        """Trigger copying path to the clipboard."""
+        QtGui.QGuiApplication.clipboard().setText(self.context_path)
+
+
+class NoItemContextMenu(FSContextMenu):
+    """Context menu when not clicking on any item."""
+
+    def __init__(self, parent):
+        """Assemble Menu."""
+        FSContextMenu.__init__(self, parent)
+
+        self.addAction(self.action_new_file)
+        self.addAction(self.action_new_folder)
+
+
+class FileContextMenu(FSContextMenu):
+    """Context menu when clicking on a file."""
+
+    def __init__(self, parent):
+        """Assemble Menu."""
+        FSContextMenu.__init__(self, parent)
+
+        self.addAction(self.action_open)
+        self.addAction(self.action_rename)
+        self.addAction(self.action_delete)
+        self.addAction(self.action_copy_path)
+
+
+class FolderContextMenu(FSContextMenu):
+    """Context menu when clicking on a folder."""
+
+    def __init__(self, parent):
+        """Assemble Menu."""
+        FSContextMenu.__init__(self, parent)
+
+        self.addAction(self.action_new_file)
+        self.addAction(self.action_new_folder)
+        self.addAction(self.action_rename)
+        self.addAction(self.action_delete)
+        self.addAction(self.setRoot)
+
+
 class FSExplorer(QtWidgets.QWidget):
     """A filesystem explorer widget."""
 
@@ -51,7 +140,7 @@ class FSExplorer(QtWidgets.QWidget):
         self.tree_view.doubleClicked.connect(self.doubleclicked)
 
         # A second filesystem model is needed for the completer because only directories should be shown as completions
-        # while both files and directories should be shown in the tree vies.
+        # while both files and directories should be shown in the tree view.
         # This duplication is somewhat unsatisfying but it was decently simple to implement and the overhead is acceptable.
         self.completer_filesystem_model = QtWidgets.QFileSystemModel()
         self.completer_filesystem_model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot)
@@ -61,6 +150,13 @@ class FSExplorer(QtWidgets.QWidget):
 
         self.Layout.addWidget(self.pathBar)
         self.Layout.addWidget(self.tree_view)
+
+        # Add Context Menu
+        self.tree_view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.tree_view.customContextMenuRequested.connect(self.rightclicked)
+        self.NoItemContextMenu = NoItemContextMenu(self)
+        self.FileContextMenu = FileContextMenu(self)
+        self.FolderContextMenu = FolderContextMenu(self)
 
         # Set initial state
         self.set_root(QtCore.QDir.homePath())
@@ -81,7 +177,7 @@ class FSExplorer(QtWidgets.QWidget):
         self.completer_filesystem_model.setRootPath(root)
         self.pathBar.setText(root)
         state.working_directory.Value = root
-        logger.info(f"Change Working Directory to '{root}'.")
+        logger.debug(f"Change Working Directory to '{root}'.")
         # self.directoryChanged.emit(root)
 
     @QtCore.Slot()
@@ -102,3 +198,89 @@ class FSExplorer(QtWidgets.QWidget):
             self.open_file.emit(path)
         if os.path.isdir(path):
             self.set_root(path)
+
+    def rightclicked(self, event):
+        """Handle right click."""
+        index = self.tree_view.indexAt(event)
+        if not index.isValid():
+            self.open_no_item_menu()
+        else:
+            path = self.filesystem_model.filePath(index)
+            if os.path.isfile(path):
+                self.open_file_menu(index)
+            if os.path.isdir(path):
+                self.open_folder_menu(index)
+
+    def open_no_item_menu(self):
+        """Open context menu when clicking on no item."""
+        path = self.filesystem_model.rootPath()
+        self.NoItemContextMenu.context_path = path
+        self.NoItemContextMenu.popup(QtGui.QCursor.pos())
+
+    def open_folder_menu(self, index):
+        """Open context menu when clicking on a folder."""
+        path = self.filesystem_model.filePath(index)
+        self.FolderContextMenu.context_path = path
+        self.FolderContextMenu.popup(QtGui.QCursor.pos())
+
+    def open_file_menu(self, index):
+        """Open context menu when clicking on a file."""
+        path = self.filesystem_model.filePath(index)
+        self.FileContextMenu.context_path = path
+        self.FileContextMenu.popup(QtGui.QCursor.pos())
+
+    #TODO: Possibly move the functions below to an own class or module as they dont directly relate to the FSExplorer
+    def new_file_in_dir(self, head_path):
+        """Prompts the user to ender a filename and creates that file in head_path."""
+        tail_path, ok = QtWidgets.QInputDialog.getText(self, "Typstwriter", "Enter filename:", QtWidgets.QLineEdit.Normal)
+        if ok and tail_path:
+            path = os.path.join(head_path, tail_path)
+            self.new_file(path)
+
+    def new_file(self, path, overwrite=False):
+        """Create new file in {path}."""
+        if not os.path.exists(path) or overwrite:
+            open(path, "w").close()
+        else:
+            logger.warn(f"File '{path}' already exists. Will not overwrite.")
+            QtWidgets.QMessageBox.warning(self, "Typstwriter", f"File '{path}' already exists.\nWill not overwrite.")
+
+    def new_folder_in_dir(self, head_path):
+        """Prompts the user to enter a foldername and creates that folder in head_path."""
+        tail_path, ok = QtWidgets.QInputDialog.getText(self, "Typstwriter", "Enter foldername:", QtWidgets.QLineEdit.Normal)
+        if ok and tail_path:
+            path = os.path.join(head_path, tail_path)
+            self.new_folder(path)
+
+    def new_folder(self, path, overwrite=False):
+        """Create new folder in {path}."""
+        if not os.path.exists(path) or overwrite:
+            os.makedirs(path)
+        else:
+            logger.warn(f"Folder '{path}' already exists. Will not overwrite.")
+            QtWidgets.QMessageBox.warning(self, "Typstwriter", f"Folder '{path}' already exists.\nWill not overwrite.")
+
+    def rename_from(self, path_from):
+        """Prompts the user to enter a new name."""
+        head_path, tail_path_from = os.path.split(path_from)
+        tail_path_to, ok = QtWidgets.QInputDialog.getText(self, "Typstwriter", "Enter new name:", QtWidgets.QLineEdit.Normal, text=tail_path_from)
+        if ok and tail_path_to:
+            path_to = os.path.join(head_path, tail_path_to)
+            self.rename(path_from, path_to)
+
+    def rename(self, path_from, path_to, overwrite=False):
+        """Rename a file or folder from path_from to path_to."""
+        if not os.path.exists(path_to) or overwrite:
+            os.rename(path_from, path_to)
+        else:
+            logger.warn(f"'{path_to}' already exists. Will not overwrite.")
+            QtWidgets.QMessageBox.warning(self, "Typstwriter", f"'{path_to}' already exists.\nWill not overwrite.")
+
+    def delete(self, path):
+        """Delete a folder or file."""
+        msg = QtWidgets.QMessageBox.question(self, "Typstwriter", f"Should '{path}' be deleted?")
+        if msg == QtWidgets.QMessageBox.StandardButton.Yes:
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                os.rmdir(path)
