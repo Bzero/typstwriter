a162fcc1b017b94454647e5c91af046cee76f456
Bzero <B_0@posteo.net>
2024-04-09 16:58:46 +0200

    Implement Live CompilerConnector_FS variant.

--- typstwriter/compiler.py
+++ typstwriter/compiler.py
@@ -15,6 +15,11 @@ state = globalstate.State
 cmode = enum.Enum("compilation_mode", ["on_demand", "live"])
 
 
+text_compiling = "compiling ..."
+text_compiled_erroniously = "compiled with errors"
+text_compiled_successfully = "compiled successfully"
+
+
 #TODO: use abc
 class CompilerConnector_FS(QtCore.QObject): # noqa: N801
     """
@@ -98,6 +103,7 @@ class CompilerConnector_FS_onDemand(CompilerConnector_FS): # noqa: N801
             self.current_stdout = ""
             self.current_stderr = ""
 
+            logger.debug("Compilation started.")
             self.compilation_started.emit()
             self.start_time = time.time()
             self.process.start()
@@ -153,9 +159,108 @@ class CompilerConnector_FS_onDemand(CompilerConnector_FS): # noqa: N801
             self.fout = fout
 
 
-# class CompilerConnector_FS_live(CompilerConnector_FS):
-#
-#     cmode = cmode.live
-#
-#     def __init__(self, fin, fout):
-#         super().__init__()
+class CompilerConnector_FS_live(CompilerConnector_FS): # noqa: N801
+    # """CompilerConnector using the filesystem and compiling live."""
+
+    cmode = cmode.live
+
+    def __init__(self, fin=None, fout=None):
+        """Initialize the connector."""
+        super().__init__()
+
+        self.compiler = config.get("Compiler", "Name")
+        self.subcommand = "watch"
+        self.fin = fin
+        self.fout = fout
+
+        self.current_stdout = None
+        self.current_stderr = None
+
+        self.process = None
+        self.recompile_scheduled = False
+
+    def start(self):
+        """Start the compiler."""
+        if self.process is None:
+            self.process = QtCore.QProcess()
+            self.process.setProgram(self.compiler)
+            self.process.setArguments([self.subcommand, self.fin, self.fout])
+            self.process.finished.connect(self.compile_finished)
+            self.process.readyReadStandardOutput.connect(self.handle_ready_stdout)
+            self.process.readyReadStandardError.connect(self.handle_ready_stderr)
+
+            self.current_stdout = ""
+            self.current_stderr = ""
+
+            self.compilation_started.emit()
+            self.start_time = time.time()
+            self.process.start()
+        else:
+            logging.warning("Attempted to start the compiler but it is already running.")
+
+    def stop(self):
+        """Stop the compiler."""
+        # Disconnect streams
+        self.process.readyReadStandardOutput.disconnect(self.handle_ready_stdout)
+        self.process.readyReadStandardError.disconnect(self.handle_ready_stderr)
+
+        # Attempt to terminate the process.
+        self.process.terminate()
+
+        #TODO: kill process if it did not terminate
+
+        # Reset state
+        self.current_stdout = None
+        self.current_stderr = None
+        self.process = None
+
+    @QtCore.Slot(int)
+    def compile_finished(self, exitcode):
+        """Cleanup if the compiler stops unexpectedly."""
+        logger.debug(f"Compiler stopped with exit code {exitcode}.")
+
+        self.current_stdout = None
+        self.current_stderr = None
+        self.process = None
+
+    def handle_ready_stdout(self):
+        """Store and Emit stdout as plain text signal."""
+        stdout = bytes(self.process.readAllStandardOutput()).decode("utf8")
+        self.current_stdout += stdout
+        self.new_stdout.emit(stdout)
+        print("STDOUT:" + stdout)
+
+    def handle_ready_stderr(self):
+        """Store and Emit stderr as plain text signal."""
+        stderr = bytes(self.process.readAllStandardError()).decode("utf8")
+        self.current_stderr += stderr
+        self.new_stderr.emit(stderr)
+
+        if text_compiling in stderr:
+            self.start_time = time.time()
+            logger.debug("Compilation started.")
+
+        if text_compiled_erroniously in stderr:
+            self.end_time = time.time()
+            logger.debug(f"Compiled {self.fin} with error in {(self.end_time - self.start_time)*1000:.2f}ms.")
+            self.compilation_finished.emit()
+
+        if text_compiled_successfully in stderr:
+            self.end_time = time.time()
+            # delta_t = parse.search("compiled successfully in {:f}ms", stderr)[0]
+            # logger.debug(f"Compiled {self.fin} successfully in {(delta_t):.2f}ms.")
+            logger.debug(f"Compiled {self.fin} successfully in {(self.end_time - self.start_time)*1000:.2f}ms.")
+            self.document_changed.emit()
+            self.compilation_finished.emit()
+
+    @QtCore.Slot(str)
+    def set_fin(self, fin):
+        """Set input file path."""
+        if isinstance(fin, str):
+            self.fin = fin
+
+    @QtCore.Slot(str)
+    def set_fout(self, fout):
+        """Set output file path."""
+        if isinstance(fout, str):
+            self.fout = fout
